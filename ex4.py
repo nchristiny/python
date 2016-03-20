5db57b1ae2a0ae8330aab8ae7b61d7c31d72f982
nchristiny <icarus747@gmail.com>
2016-03-20 16:49:45 -0500

    Initial commit

---
+++ ex4.py
@@ -0,0 +1,14 @@
+my_name = "Nick Christiny"
+my_age = 36
+my_height = 67
+my_weight = 170
+my_eyes = 'Brown'
+my_teeth = 'Red'
+my_hair = 'Dark Brown'
+
+print "Let's talk about %r." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
